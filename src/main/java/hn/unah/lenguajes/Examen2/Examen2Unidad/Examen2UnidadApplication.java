2e98237acd3572f12d3aafcd76fbe5ac85a4fe9b
BryanMejia1 <bryanstevenmejia873@gmail.com>
2024-04-05 16:31:20 -1100

    primer push

---
+++ src/main/java/hn/unah/lenguajes/Examen2/Examen2Unidad/Examen2UnidadApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes.Examen2.Examen2Unidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2UnidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2UnidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hn/unah/lenguajes/Examen2/Examen2Unidad/Examen2UnidadApplicationTests.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes.Examen2.Examen2Unidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2UnidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
